760cb1af7a730c7b8141727bf82cdf91c19e6d02
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 21:32:43 -0800

    Exposure mode

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import numpy as np
 from picamera import PiCamera
 print("started")
 camera = PiCamera()
-camera.contrast = 50
+camera.exposure_mode = 'auto'
 camera.capture('image1.jpg')
 camera.close()
