e5ef20a6419edd4dd908ce5f085a873caeacb62d
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 21:11:42 -0800

    Add image capture functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from picamera import PiCamera
 print("started")
 camera = PiCamera()
 print(camera.ISO)
+camera.capture('image0.jpg')
 camera.ISO = 800
-camera.capture('image.jpg')
+camera.capture('image1.jpg')
 print(camera.ISO)
