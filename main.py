e7cb152607c2f2922e27af5be11993271a816873
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 22:05:18 -0800

    Update camera settings in main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import numpy as np
 from picamera import PiCamera
+from time import sleep
 print("started")
 camera = PiCamera()
-camera.exposure_mode = 'auto'
+camera.iso = 200
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+camera.resolution = (1024, 768)
+#camera.exposure_mode = 'auto'
+#camera.awb_mode = 'auto'
+camera.start_preview()
+sleep(2)
 camera.capture('image1.jpg')
 camera.close()
