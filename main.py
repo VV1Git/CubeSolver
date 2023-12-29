58e5534ace027b5dc3dc91364a4b69e69b3188a4
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 21:24:02 -0800

    Update camera exposure mode

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import numpy as np
 from picamera import PiCamera
 print("started")
 camera = PiCamera()
-camera.brightness = 90
+camera.exposure_mode = "verylong"
 camera.capture('image1.jpg')
 camera.close()
