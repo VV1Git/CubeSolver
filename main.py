dc802d020a4e16e6f1932c49a94dd7f1f2f502c8
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 22:06:36 -0800

    Update camera settings

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from picamera import PiCamera
 from time import sleep
 print("started")
 camera = PiCamera()
-camera.iso = 200
+"""camera.iso = 200
 camera.shutter_speed = camera.exposure_speed
 camera.exposure_mode = 'off'
 g = camera.awb_gains
 camera.awb_mode = 'off'
-camera.awb_gains = g
+camera.awb_gains = g"""
 camera.resolution = (1024, 768)
-#camera.exposure_mode = 'auto'
-#camera.awb_mode = 'auto'
+camera.exposure_mode = 'auto'
+camera.awb_mode = 'auto'
 camera.start_preview()
 sleep(2)
 camera.capture('image1.jpg')
