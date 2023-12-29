b4879ae6f07b2cbab1af692b82f0b32e29c892ff
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-28 21:26:43 -0800

    Change contrast

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import numpy as np
 from picamera import PiCamera
 print("started")
 camera = PiCamera()
-camera.exposure_mode = "verylong"
+camera.contrast = 50
 camera.capture('image1.jpg')
 camera.close()
