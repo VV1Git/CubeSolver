46e834612d40e4b535775f7be3454e51956c780d
VV1Git <vikramaditya.vedant@gmail.com>
2023-12-30 19:47:10 -0800

    Stepper motor control

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import numpy as np
 from picamera import PiCamera
-from time import sleep
+import kociemba
+import RPi.GPIO as GPIO
+import time
+
+"""kociemba.solve('UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB')
 print("started")
 camera = PiCamera()
-"""camera.iso = 200
+camera.iso = 200
 camera.shutter_speed = camera.exposure_speed
 camera.exposure_mode = 'off'
 g = camera.awb_gains
 camera.awb_mode = 'off'
-camera.awb_gains = g"""
+camera.awb_gains = g
 camera.led = False
 camera.resolution = (1024, 768)
 camera.exposure_mode = 'auto'
@@ -16,4 +20,71 @@ camera.awb_mode = 'auto'
 camera.start_preview()
 sleep(2)
 camera.capture('image1.jpg')
-camera.close()
+camera.close()"""
+
+ 
+out1 = 26
+out2 = 19
+out3 = 13
+out4 = 6
+ 
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+ 
+step_count = 200
+ 
+# setting up
+GPIO.setmode( GPIO.BCM )
+GPIO.setup( out1, GPIO.OUT )
+GPIO.setup( out2, GPIO.OUT )
+GPIO.setup( out3, GPIO.OUT )
+GPIO.setup( out4, GPIO.OUT )
+ 
+# initializing
+GPIO.output( out1, GPIO.LOW )
+GPIO.output( out2, GPIO.LOW )
+GPIO.output( out3, GPIO.LOW )
+GPIO.output( out4, GPIO.LOW )
+ 
+ 
+def cleanup():
+    GPIO.output( out1, GPIO.LOW )
+    GPIO.output( out2, GPIO.LOW )
+    GPIO.output( out3, GPIO.LOW )
+    GPIO.output( out4, GPIO.LOW )
+    GPIO.cleanup()
+ 
+ 
+# the meat
+try:
+    i = 0
+    for i in range(step_count):
+        if i%4==0:
+            GPIO.output( out4, GPIO.HIGH )
+            GPIO.output( out3, GPIO.LOW )
+            GPIO.output( out2, GPIO.LOW )
+            GPIO.output( out1, GPIO.LOW )
+        elif i%4==1:
+            GPIO.output( out4, GPIO.LOW )
+            GPIO.output( out3, GPIO.LOW )
+            GPIO.output( out2, GPIO.HIGH )
+            GPIO.output( out1, GPIO.LOW )
+        elif i%4==2:
+            GPIO.output( out4, GPIO.LOW )
+            GPIO.output( out3, GPIO.HIGH )
+            GPIO.output( out2, GPIO.LOW )
+            GPIO.output( out1, GPIO.LOW )
+        elif i%4==3:
+            GPIO.output( out4, GPIO.LOW )
+            GPIO.output( out3, GPIO.LOW )
+            GPIO.output( out2, GPIO.LOW )
+            GPIO.output( out1, GPIO.HIGH )
+ 
+        time.sleep( step_sleep )
+ 
+except KeyboardInterrupt:
+    cleanup()
+    exit( 1 )
+ 
+cleanup()
+exit( 0 )
